0b1f81b427534638d37162643f0dd1a1f3e5432f
Elaine Cadman <elaine.cadman@gmail.com>
2018-12-21 20:58:00 -0800

    foundation

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Veggie Burger with avocado", false),
+("Chipotle Pork Burger with meunster", false),
+("Salmon Burger with soy and green onions", false);
